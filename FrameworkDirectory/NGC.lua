523fde8519ddcc1c925e55d3ef157022a9f90cc2
HenriqueLua <89322086+HenriqueLua@users.noreply.github.com>
2022-01-28 18:30:09 -0300

    Update: Events/Signals

--- FrameworkDirectory/NGC.lua
+++ FrameworkDirectory/NGC.lua
@@ -26,40 +26,43 @@ local Framework: Framework = {
 
 function Framework:ConnectBridge( ): RBXScriptSignal?
 	local function initializationBridge(module: ModuleScript)
-		
+
 		local module_ = require(module)
 
 		-- #Running:
 
 		Packets.Promise.new(function(resolve, onCancel, reject)
-			if (RunService:IsServer()) then resolve() end
-		end):andThen(function()
-			module_.Running()
+			if (RunService:IsServer()) then resolve("Server") elseif (RunService:IsClient()) then resolve("Client") end
+		end):andThen(function(Face)
+
+			module_:Running() -- [Start Running]
+
+			-- #AfterRunning:
+
+			Packets.Promise.new(function(resolve, onCancel, reject)
+				if (Face == "Client") then if (game:IsLoaded()) then resolve() elseif (not game:IsLoaded()) then game.Loaded:Wait() resolve() end
+				elseif (Face == "Server") then
+					resolve()
+				end
+			end):andThen(function()
+				module_:AfterRunning()
+			end)
+
+
 		end)
-
-		-- #AfterRunning:
-
-		Packets.Promise.new(function(resolve, onCancel, reject)
-			if (RunService:IsServer()) then 
-				if (game:IsLoaded()) then resolve() elseif (not game:IsLoaded()) then game.Loaded:Wait() resolve() end
-			end
-		end):andThen(function()
-			module_.AfterRunning()
-		end)
-
 
 	end
 	return {
 		ConnectServices = function(Folder: Folder | Instance) 
 			assert(typeof(Folder) == "Instance", "Error! Folder (ConnectServices(Folder)) is not of type 'Folder'!")
 			local ListOfServices = { }
-			for _Index, Elements in ipairs(Folder:GetChildren()) do print(Elements) table.insert(ListOfServices, Elements); initializationBridge(Elements) end
+			for _Index, Elements in pairs(Folder:GetChildren()) do table.insert(ListOfServices, Elements); initializationBridge(Elements) end
 			Framework.Services = ListOfServices --[Add services in table 'Services']
 		end;
 		ConnectUsufruidores = function(Folder: Folder | Instance) 
 			assert(typeof(Folder) == "Instance", "Error! Folder (ConnectServices(Folder)) is not of type 'Folder'!")
 			local ListOfServices = { }
-			for _Index, Elements in ipairs(Folder:GetChildren()) do table.insert(ListOfServices, Elements) end
+			for _Index, Elements in pairs(Folder:GetChildren()) do table.insert(ListOfServices, Elements); initializationBridge(Elements) end
 			Framework.Usufruidores = ListOfServices --[Add services in table 'Usufruidores']
 		end;
 	}
@@ -67,7 +70,6 @@ end
 
 
 function Framework:Create(Data: table)
-	assert(typeof(Data) == "table", "Data (Framework:Create(Data: table)) is not a table!")
 	Packets.Promise.new(function(resolve, onCancel, reject)
 		if (RunService:IsClient()) then
 			Packets.Promise.new(function(resolve, onCancel, reject)
@@ -79,8 +81,15 @@ function Framework:Create(Data: table)
 			end):andThen(function(Entry: boolean)
 				if (Entry) then  
 
-					table.insert(Framework.UsufruidoresStorage, {["Name"] = Data.Name --[[Table]], StorageData = Data.StorageData --[[Table]]})
+					Framework.UsufruidoresStorage[Data.Name] = { Name = Data.Name, StorageData = Data.StorageData, Events = Data.Events }
+					
+					local DataUse = Framework.UsufruidoresStorage[Data.Name]
 
+					for Index = 1, #DataUse.Events do
+						local Signal = DataUse.Events[Index]
+						CreateSignal(Signal[1], Signal[2], Signal[3], Signal[4], DataUse)
+					end
+					
 				elseif (not Entry) then error("'Entry > true'expected, but, is 'false'! [Usufruidores List do not loaded!]") end
 			end)
 		elseif (RunService:IsServer()) then
@@ -93,13 +102,64 @@ function Framework:Create(Data: table)
 			end):andThen(function(Entry: boolean)
 				if (Entry) then  
 
-					table.insert(Framework.ServicesStorage, {["Name"] = Data.Name, StorageData = Data.StorageData})
-
+					Framework.ServicesStorage[Data.Name] = {Name = Data.Name, StorageData = Data.StorageData, Events = Data.Events}
+					
+					local DataUse = Framework.ServicesStorage[Data.Name]
+					
+					for Index = 1, #DataUse.Events do
+						local Signal = DataUse.Events[Index]
+						CreateSignal(Signal[1], Signal[2], Signal[3], Signal[4], DataUse)
+					end
+					
 				elseif (not Entry) then error("'Entry > true'expected, but, is 'false'! [Services List do not loaded!]") end
 			end)
 		end
 	end)
-	return { ["Name"] = Data.Name, StorageData = Data.StorageData } --[For use your index after called!]
+	if (RunService:IsServer()) then return Framework.ServicesStorage[Data.Name] elseif (RunService:IsClient()) then return Framework.UsufruidoresStorage[Data.Name] end
+end
+
+--[[
+
+CreateSignal:
+
+  	Events = {
+		Example = { "Nome do seu evento (Example)", "BindableEvent" (Tipo do evento), 
+		"Service" (Nome do Usufruidor/Serviço), script (Coloque o seu index/ModuleScript aqui) }
+	};
+
+
+]]
+
+function CreateSignal( NameSignal, NameEvent: string, NameIndex: string, Index: ModuleScript, DataUse )
+	Packets.Promise.new(function(resolve, onCancel, reject)
+		local Signals: Folder, Name = Instance.new("Folder"), "Signals"
+		local SignalsTypes = { 
+
+			-- [Events Types]
+
+			BindableEvent = {Instance = Instance.new("BindableEvent"), Type = "BindableEvent"};
+			RemoteEvent = {Instance = Instance.new("RemoteEvent"), Type = "RemoteEvent"};
+
+			-- [Functions Types]
+
+			BindableFunction = {Instance = Instance.new("BindableFunction"), Type = "BindableFunction"};
+			RemoteFunction = {Instance = Instance.new("RemoteFunction"), Type = "RemoteFunction"};
+
+		}
+
+		if (not Index:FindFirstChild("Signals")) then Signals.Parent = (Index); Signals.Name = (Name) end
+
+		local Item = SignalsTypes[NameEvent]
+		local Signal, Type = Item.Instance:Clone(), Item.Type; 
+
+		Signal.Parent = Index:WaitForChild(Name);
+		Signal.Name = NameSignal;
+		
+		resolve(Signal, Type)
+		
+	end):andThen(function( Signal, Type )
+		table.insert(DataUse.Events.EventsDeep, { [NameEvent] = {Instance = Signal, Type = Type} })
+	end)
 end
 
 return Framework
--- README.md
+++ README.md
@@ -1,5 +1,5 @@
-#           **_NGC - Framework_**
-_______________________________________________
+                    #           **_NGC - Framework_**
+_______________________________________________________________________________________
 
 ___° Variadades de variáveis de entradas do rótulo principal:___
 
@@ -18,6 +18,10 @@ local Service = NGC.CreateService{
 
     Name = "Nome do seu 'Service'";
     StorageData = { };
+    Events = {
+        -- [Crie seus próprios eventos]
+        EventsDeep = {}; -- [Armazenamento profundo. Aqui ficará seus eventos para que você possa utiliza-los novamente!]
+    };
 
     -- > ["BETA VARIABLES!"] < -- 
 
@@ -62,6 +66,10 @@ local Usufruidor = NGC.CreateUsufruidor{
 
     Name = "Nome do seu 'Usufruidor'";
     StorageData = { };
+    Events = {
+        -- [Crie seus próprios eventos]
+        EventsDeep = {}; -- [Armazenamento profundo. Aqui ficará seus eventos para que você possa utiliza-los novamente!]
+    };
 
     -- > ["BETA VARIABLES!"] < -- 
 
@@ -89,5 +97,38 @@ end
 
 Service:DeferLive{Date = {Second, Minute, Hour, Day, ...}}
 
+
+```
+
+##         **_Criação de eventos e armazenamentos de eventos profundos:_**
+_______________________________________________________________________________________
+
+
+# _Como funciona a criação de um evento?_
+  
+  Um evento é criado assim que adicionado uma tabela de eventos, que quando ela é adicionada e contém pelo menos um evento então automaticamente uma pasta de armazenamentos externo e interno são criados. Para facilitar a criação de um evento para quaisquer tipo de programador "Lua" eu irei vindo atulizando o sistema sempre e, adicionando novas ferramentas para possibilitar ao programador uma extensão exclusiva e potente!
+
+# _Por que devo utilizar esse sistema para a criação de eventos?_
+
+ Normalmente essa ferramenta contém algumas opções extras no desenvolvimento de funções do seu jogo. 
+  Teremos opções para lacrar/trancar a possibilidade de entrada do cliente ao servidor e vire versa. Além disso teremos opções como exemplo: aprimoração de desempenho (Que irá ajudar no código como exemplo: avisar dicas do que você poderia usar para manter o código limpo, adicionar funções automáticas para melhorá o desempenho e até irá lhe informar caso o Roblox adicione alguma função extra no futuro).
+
+# _Como você deve usá-lo de forma adequada:_
+
+```lua
+
+-- [Preocupe-se de fazer isso dentro de uma criação de um usufruidor/service]
+
+Events = {
+
+    -- [Nunca se esqueça de fazer uma enumeração de seus eventos/sinais]
+
+    [1] = {"Nome do evento", "Tipo do evento como exemplo: BindableEvent", "Nome do usufruidor/service", script --[[(Certifique-se de colocar o script que está criando isso)]]}
+
+    --[Em desenvolvimento/explicado em breve].
+
+    EventsDeep = {}
+
+};
 
 ```
