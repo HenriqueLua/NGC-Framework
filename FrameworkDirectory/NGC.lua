4dfc6a5e50e9f0874b96232aaab9b813c959affc
HenriqueLua <89322086+HenriqueLua@users.noreply.github.com>
2022-01-28 08:49:34 -0300

    Update 0.0.002

--- FrameworkDirectory/NGC.lua
+++ FrameworkDirectory/NGC.lua
@@ -3,23 +3,57 @@ type Framework = {
 	Usufruidores: Folder;
 }
 
+type Packets = {
+	Promise : ModuleScript? | any;
+}
+
+local ModuleScript: ModuleScript = script
+
+local RunService: RunService = game:GetService("RunService")
+
+local Packets: Packets = {
+	Promise = require(Module.Parent:WaitForChild("Packets").Promise);
+} 
+
 local Framework: Framework = {
 	
 	-- @Folders accessible storages (Client & Server):
 
-	Services = {};
-	Usufruidores = {};
+	Services = {}; ServicesStorage = {};
+	Usufruidores = {}; UsufruidoresStorage = {};
 
 }
 
 function Framework:ConnectBridge( ): RBXScriptSignal?
-	local function initializationBridge()
+	local function initializationBridge(module: ModuleScript)
+
+		local module_ = require(module)
+
+		-- #Running:
+
+		Promise.new(function(resolve, onCancel, reject)
+			if (RunService:IsServer()) then resolve() end
+	    end):andThen(function()
+	        module_.Running()
+	    end)
+
+	    -- #AfterRunning:
+
+	    Promise.new(function(resolve, onCancel, reject)
+			if (RunService:IsServer()) then 
+				if (game.IsLoaded()) then resolve() elseif (not game.IsLoaded()) then game.Loaded:Wait() resolve() end
+			end
+	    end):andThen(function()
+	        module_.AfterRunning()
+	    end)
+
+
 	end
 	return {
 		ConnectServices = function(Folder: Folder | Instance) 
 			assert(type(Folder) == "Folder", "Error! Folder (ConnectServices(Folder)) is not of type 'Folder'!")
 	        local ListOfServices = { }
-	        for _Index, Elements in ipairs(Folder:GetChildren()) do table.insert(ListOfServices, Elements) end
+	        for _Index, Elements in ipairs(Folder:GetChildren()) do table.insert(ListOfServices, Elements); initializationBridge(Elements) end
 	        Framework.Services = ListOfServices --[Add services in table 'Services']
 		end;
 		ConnectUsufruidores = function(Folder: Folder | Instance) 
@@ -31,6 +65,39 @@ function Framework:ConnectBridge( ): RBXScriptSignal?
 	}
 end
 
+
+function Framework:Create(Data: table)
+	assert(typeof(Data) == "table", "Data (Framework:Create(Data: table)) is not a table!")
+	if (RunService:IsClient()) then
+		Promise.try(function(resolve, onCancel, reject)
+			if (Framework.Usufruidores) then
+				resolve(true)
+			elseif (not Framework.Usufruidores) then
+				resolve(false)
+			end
+		end):andThen(function(Entry: boolean)
+		    if (Entry) then  
+
+		    	table.insert(Framework.UsufruidoresStorage, {["Name"] = Data.Name --[[Table]], StorageData = Data.StorageData --[[Table]]})
+
+		    elseif (not Entry) then error("'Entry > true'expected, but, is 'false'! [Usufruidores List do not loaded!]") end
+	    end)
+	elseif (RunService:IsServer()) then
+		Promise.try(function(resolve, onCancel, reject)
+			if (Framework.Services) then
+				resolve(true)
+			elseif (not Framework.Services) then
+				resolve(false)
+			end
+		end):andThen(function(Entry: boolean)
+		    if (Entry) then  
+
+		    	table.insert(Framework.ServicesStorage, {["Name"] = Data.Name, StorageData = Data.StorageData})
+
+		    elseif (not Entry) then error("'Entry > true'expected, but, is 'false'! [Services List do not loaded!]") end
+	    end)
+	end
+end
 
 
 return Framework
